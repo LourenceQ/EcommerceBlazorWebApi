59ba93b6437521dd52b9a1841812ef6b3719a5a1
LourenceQ <lqfmecatronica@gmail.com>
2022-07-29 14:15:57 -0300

    Implementing new Gategory response in the client

---
+++ EcommerceBlazorWebApi/Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,21 @@
+﻿namespace EcommerceBlazorWebApi.Client.Services.CategoryService;
+
+public class CategoryService : ICategoryService
+{
+    private readonly HttpClient _http;
+
+    public CategoryService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public IReadOnlyList<Category> Categories { get; set; } = new List<Category>();  
+
+    public async Task GetCategories()
+    {
+        var response = await _http
+            .GetFromJsonAsync<ServiceResponse<IReadOnlyList<Category>>>("api/category");
+        if(response != null && response.Data!= null)
+            Categories = response.Data;
+    }
+}
---
+++ EcommerceBlazorWebApi/Client/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,7 @@
+﻿namespace EcommerceBlazorWebApi.Client.Services.CategoryService;
+
+public interface ICategoryService
+{
+    IReadOnlyList<Category> Categories { get; set; }
+    Task GetCategories();
+}
