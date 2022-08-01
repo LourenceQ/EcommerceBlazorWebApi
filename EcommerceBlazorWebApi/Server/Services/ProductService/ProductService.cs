635c5e0864e1bebca5eb87c2014f025240707dce
LourenceQ <lqfmecatronica@gmail.com>
2022-08-01 18:22:12 -0300

    Rrefactoring Product Service to include ProductTypes and Variants

--- EcommerceBlazorWebApi/Server/Services/ProductService/ProductService.cs
+++ EcommerceBlazorWebApi/Server/Services/ProductService/ProductService.cs
@@ -12,7 +12,11 @@ public class ProductService : IProductService
     public async Task<ServiceResponse<Product>> GetProductByIdAsync(int productId)
     {
         var response = new ServiceResponse<Product>();
-        var product = await _context.Products.FindAsync(productId);
+        var product = await _context.Products
+            .Include(p => p.Variants)
+            .ThenInclude(v => v.ProductType)
+            .FirstOrDefaultAsync(p => p.Id == productId);
+
         if(product == null)
         {
             response.Success = false;
@@ -30,7 +34,9 @@ public class ProductService : IProductService
     {
         var response = new ServiceResponse<IReadOnlyList<Product>>()
         {
-            Data = await _context.Products.ToListAsync()
+            Data = await _context.Products
+                .Include(p => p.Variants)   
+                .ToListAsync()
         };
 
         return response;
@@ -43,6 +49,7 @@ public class ProductService : IProductService
         {
             Data = await _context.Products.Where(p => 
                 p.Category!.Url.ToLower().Equals(categoryUrl.ToLower()))
+                .Include(p => p.Variants)
                 .ToListAsync()  
         };
 
