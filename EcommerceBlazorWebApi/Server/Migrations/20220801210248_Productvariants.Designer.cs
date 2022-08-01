e14bdb0605665d56d30539038687ffacbeba4368
LourenceQ <lqfmecatronica@gmail.com>
2022-08-01 18:09:42 -0300

    Adding new migations and seeding database qith productTypes and productVariants

--- EcommerceBlazorWebApi/Server/Migrations/20220801210248_Productvariants.Designer.cs
+++ EcommerceBlazorWebApi/Server/Migrations/20220801210248_Productvariants.Designer.cs
@@ -3,6 +3,7 @@ using EcommerceBlazorWebApi.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EcommerceBlazorWebApi.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220801210248_Productvariants")]
+    partial class Productvariants
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
