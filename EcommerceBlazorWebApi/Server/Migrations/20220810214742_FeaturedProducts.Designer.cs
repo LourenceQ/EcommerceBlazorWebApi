b182a1b15377aab853d1cd2be892c1a7666a2ece
LourenceQ <lqfmecatronica@gmail.com>
2022-08-10 18:53:24 -0300

    Implementing featured products property with new migration

--- EcommerceBlazorWebApi/Server/Migrations/20220810214742_FeaturedProducts.Designer.cs
+++ EcommerceBlazorWebApi/Server/Migrations/20220810214742_FeaturedProducts.Designer.cs
@@ -3,6 +3,7 @@ using EcommerceBlazorWebApi.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EcommerceBlazorWebApi.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220810214742_FeaturedProducts")]
+    partial class FeaturedProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
