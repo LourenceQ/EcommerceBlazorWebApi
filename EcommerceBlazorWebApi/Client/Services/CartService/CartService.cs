c7e6e59dde50d999700e4767499c9374486e6b31
LourenceQ <lqfmecatronica@gmail.com>
2022-08-18 23:18:54 -0300

    Implementing GetCartProducts interface in the client

--- EcommerceBlazorWebApi/Client/Services/CartService/CartService.cs
+++ EcommerceBlazorWebApi/Client/Services/CartService/CartService.cs
@@ -5,10 +5,12 @@ namespace EcommerceBlazorWebApi.Client.Services.CartService;
 public class CartService : ICartService
 {
     private readonly ILocalStorageService _localStorage;
+    private readonly HttpClient _http;
 
-    public CartService(ILocalStorageService localStorage)
+    public CartService(ILocalStorageService localStorage, HttpClient http)
     {
         _localStorage = localStorage;
+        _http = http;
     }
 
     public event Action? OnChange;
@@ -34,5 +36,18 @@ public class CartService : ICartService
             cart = new List<CartItem>();
         }
         return cart;
+    }
+
+    public async Task<IReadOnlyList<CartProductResponseDto>> GetCartProducts()
+    {
+        var cartItems = await _localStorage.GetItemAsync<IReadOnlyList<CartItem>>("cart");
+
+        var response = await _http.PostAsJsonAsync("api/cart;products", cartItems);
+
+        var cartProducts = await response
+            .Content.ReadFromJsonAsync<ServiceResponse<IReadOnlyList<CartProductResponseDto>>>();
+
+        return cartProducts.Data;
+
     }
 }
